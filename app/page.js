869d383e5e8d058c560a8274a646c5acef78a9e4
Vamnaone <217135027+assistant-support@users.noreply.github.com>
2025-09-03 07:44:03 +0700

    d

--- app/page.js
+++ app/page.js
@@ -1,25 +1,10 @@
-// app/test/page.js
-
-import { Button } from "@/components/ui/button";
+'use client'
+import { redirect } from "next/navigation";
 
 export default function TestPage() {
+    redirect("/client");
     return (
         <div className="min-h-screen bg-background flex flex-col items-center justify-center gap-4 p-8">
-            <h1 className="text-2xl font-bold text-foreground">
-                Trang Test cho Shadcn UI
-            </h1>
-            <p className="text-muted-foreground">
-                Nếu các button dưới đây hiển thị đúng style, vấn đề là do xung đột với MUI.
-            </p>
-
-            <div className="flex gap-4">
-                <Button>Nút Mặc Định</Button>
-                <Button variant="destructive">Nút Hủy</Button>
-                <Button variant="outline">Nút Outline</Button>
-                <Button variant="secondary">Nút Phụ</Button>
-                <Button variant="ghost">Nút Ghost</Button>
-                <Button variant="link">Nút Link</Button>
-            </div>
         </div>
     );
 }
